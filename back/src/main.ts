bc1781c4f01595ecad25ab135b2a1d7680056212
Laura Ochoa <65990018+Loes75@users.noreply.github.com>
2023-05-27 00:15:39 -0500

    connect app to firebase

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as firebaseAdmin from 'firebase-admin';
+import { firebaseConfig } from './firebase.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  firebaseAdmin.initializeApp(firebaseConfig);
   await app.listen(3000);
 }
 bootstrap();
